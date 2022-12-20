0cd85c433b128bdfb5e9a3dfe51d0243365b96ba
yamesproject <iyan10.gts@gmail.com>
2022-12-20 13:35:53 +0700

    upload to github

---
+++ pratista/app/Http/Controllers/SekolahController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SekolahController extends Controller
+{
+    public function fsiswa() {
+        return view('bsiswa');
+    }
+}
